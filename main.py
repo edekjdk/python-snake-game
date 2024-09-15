b2133b3327afca35db9d02d4edaf9ecaa8023cc3
Kacper <luszczkacper42@gmail.com>
2024-09-15 13:55:07 +0200

    added apple basic functionality

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 
 class Game:
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((1000, 500))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 10)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
 
     def run(self):
         running = True
@@ -28,23 +35,26 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.walk()
+            self.play()
             time.sleep(0.2)
 
 
 class Snake:
-    def __init__(self, surface):
+    def __init__(self, surface, length):
         self.block = pygame.Surface((20, 20))
-        self.block.fill((255, 0, 0))
-        self.x = 100
-        self.y = 100
-        self.direction = "left"
+        self.block.fill((0, 255, 0))
+        self.length = length
+        self.x = [100] * self.length
+        self.y = [100] * self.length
+        self.direction = "right"
         self.surface = surface
 
     def draw(self):
         self.surface.fill((0, 0, 0))
-        self.surface.blit(self.block, (self.x, self.y))
-        pygame.display.flip()
+
+        for i in range(self.length):
+            self.surface.blit(self.block, (self.x[i], self.y[i]))
+            pygame.display.flip()
 
     def moveUp(self):
         self.direction = "up"
@@ -59,16 +69,35 @@ class Snake:
         self.direction = "right"
 
     def walk(self):
+
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
         if self.direction == "left":
-            self.x -= 10
+            self.x[0] -= 20
         if self.direction == "right":
-            self.x += 10
+            self.x[0] += 20
         if self.direction == "up":
-            self.y -= 10
+            self.y[0] -= 20
         if self.direction == "down":
-            self.y += 10
+            self.y[0] += 20
         self.draw()
-www
+
+
+
+class Apple:
+    def __init__(self, surface):
+        self.apple = pygame.Surface((20, 20))
+        self.apple.fill((255, 0, 0))
+        self.surface = surface
+        self.x = random.randint(1, 999)
+        self.y = random.randint(1, 499)
+
+    def draw(self):
+        self.surface.blit(self.apple, (self.x, self.y))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     game = Game()
